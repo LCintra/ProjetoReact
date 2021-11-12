f91d4f620996a6fa691e86442c8e768997d1f0a3
limmatamara <limmatamara@gmail.com>
2021-11-12 17:47:23 -0300

    Uso do Formik

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,29 +1,45 @@
 import SectionTitle from "../SectionTitle/SectionTitle";
 import styles from './Register.module.css'
+import { useFormik } from "formik";
+
 const Register = () =>{
+  const formik = useFormik ({
+    initialValues:
+    {
+      firstName: '',
+      lastName: '',
+      email: '',
+      address: '',
+      phone: ''
+    }, 
+    onSubmit: values => {
+      console.log(values);
+    }
+  })
+
   return(
   <div className="container">
     <SectionTitle titulo="Cadastro"/>
-    <form className={styles.formsMainDiv}>
+    <form onSubmit = {formik.handleSubmit} className={styles.formsMainDiv}>
       <div className={styles.inputDiv}>
-        <label for="nome-completo">Primeiro Nome*</label>
-        <input name="nome-completo" type="text"></input>
+        <label htmlFor="firstName">Primeiro Nome*</label>
+        <input value={formik.values.firstName} onChange={formik.handleChange} name="firstName" type="text"></input>
       </div>
       <div className={styles.inputDiv}>
-        <label for="data-nascimento">Último Nome*</label>
-        <input type="text"></input>
+        <label htmlFor="lastName">Último Nome*</label>
+        <input value={formik.values.lastName} onChange={formik.handleChange} type="text" name="lastName"></input>
       </div>
       <div className={styles.inputDiv}>
-        <label for="email">Email*</label>
-        <input name="email" type="email"></input>
+        <label htmlFor="email">Email*</label>
+        <input value={formik.values.email} onChange={formik.handleChange} name="email" type="email"></input>
       </div>
       <div className={styles.inputDiv}>
-        <label for="senha">Endereço*</label>
-        <input type="password" name="senha"></input>
+        <label htmlFor="address">Endereço*</label>
+        <input value={formik.values.address} onChange={formik.handleChange} type="text" name="address"></input>
       </div>
       <div className={styles.inputDiv}>
-        <label for="senha">Telefone*</label>
-        <input type="password" name="senha"></input>
+        <label htmlFor="phone">Telefone*</label>
+        <input value={formik.values.phone} onChange={formik.handleChange} type="number" name="phone"></input>
       </div>
       <div className={styles.inputDiv}>
         <button className={styles.formButton} type="submit">Cadastrar</button>
